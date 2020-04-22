b8f7c5dbae29dcbb48c66fed1157695865855f34
sanchit2107 <sanchit2107@gmail.com>
2020-04-22 20:42:52 +0530

    Spring Boot Web Demo Implementation

---
+++ SpringBootWebDemo3/src/main/java/org/com/SpringBootWebDemo3Application.java
@@ -0,0 +1,13 @@
+package org.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebDemo3Application.class, args);
+	}
+
+}
