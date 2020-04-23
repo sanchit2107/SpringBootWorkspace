1ffb8be9edbe7b2e71328dba7c682a80f2d1324d
sanchit2107 <sanchit2107@gmail.com>
2020-04-23 17:05:14 +0530

    Changes in Product COntroller-- for Angular add CrossOrigin for 4200

--- RestApplicationDemo1/src/main/java/org/com/controller/ProductController.java
+++ RestApplicationDemo1/src/main/java/org/com/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.com.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/product")
 public class ProductController {
